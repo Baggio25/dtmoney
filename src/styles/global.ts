a6ca30f9fde7e535be63dfaf93f710eb652d00b8
Rodrigo Baggio <rodrigo.baggio.si@gmail.com>
2021-12-15 13:43:35 -0300

    Header layout created

---
+++ src/styles/global.ts
@@ -0,0 +1,48 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  
+  :root{
+    --background: #f0f2f5;
+    --red: #E52e54;
+    --blue: #5429CC;
+    --green: #33CC95;
+
+    --blue-light: #6933FF;
+
+    --text-title: #363f5f;
+    --text-body: #969cb3;
+
+    --shape: #FFFFFF
+}
+  
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  
+  body {
+    background-color: var(--background);
+    -webkit-font-smoothing: antialiased; // fonte mais detalhada
+  }
+
+  body, input, textarea, button {
+    font-family: 'Poppins', sans-serif;
+    font-weight: 400;
+  }
+
+  h1, h2, h3, h4, h5, h6, strong {
+    font-weight: 600;
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  [disabled] {
+    opacity: 0.6;
+    curor: not-allowed;
+  }
+
+`;
