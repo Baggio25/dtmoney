de74736705200784486fbf6d37d3d936234fbbfa
Rodrigo Baggio <rodrigo.baggio.si@gmail.com>
2021-12-16 11:02:16 -0300

    Create Type Buttons

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Modal from "react-modal";
 
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
+import { NewTransactionModal } from "./components/NewTransactionModal/index";
 
 import { GlobalStyle } from "./styles/global";
 
@@ -26,12 +27,10 @@ export function App() {
 
       <Dashboard />
 
-      <Modal
+      <NewTransactionModal
         isOpen={isNewTransactionModalOpen}
         onRequestClose={handleCloseNewTransactionModal}
-      >
-        <h2>Cadastrar transação</h2>
-      </Modal>
+      />
 
       <GlobalStyle />
     </>
